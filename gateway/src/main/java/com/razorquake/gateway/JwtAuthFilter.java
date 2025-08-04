d67d9520d874e15d7127b2348c92aa636b542863
Anant Jaiswal <razorquake2499@gmail.com>
2025-08-04 18:41:12 +0530

    Removed unused imports

--- gateway/src/main/java/com/razorquake/gateway/JwtAuthFilter.java
+++ gateway/src/main/java/com/razorquake/gateway/JwtAuthFilter.java
@@ -2,7 +2,6 @@ package com.razorquake.gateway;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
