3d354288a16fd86dd161ceaa4ac8da93f7edcad1
Anant Jaiswal <razorquake2499@gmail.com>
2025-08-07 15:54:40 +0530

    Implemented Async Communication & migrated to cloud amqp

--- order/src/main/java/com/razorquake/order/service/OrderService.java
+++ order/src/main/java/com/razorquake/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.razorquake.order.service;
 
+import com.razorquake.order.dto.OrderCreatedEvent;
 import com.razorquake.order.dto.OrderItemDTO;
 import com.razorquake.order.dto.OrderResponse;
 import com.razorquake.order.model.CartItem;
@@ -8,10 +9,12 @@ import com.razorquake.order.model.Order;
 import com.razorquake.order.model.OrderItem;
 import com.razorquake.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -19,6 +22,8 @@ import java.util.Optional;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final CartService cartService;
+    private final RabbitTemplate rabbitTemplate;
+
     public Optional<OrderResponse> createOrder(String userId) {
         List<CartItem> cartItems = cartService.getCartItems(userId);
         if (cartItems.isEmpty()) {
@@ -53,7 +58,31 @@ public class OrderService {
 
         cartService.clearCart(userId);  // Clear the cart after order creation
 
+        rabbitTemplate.convertAndSend(
+                "order.exchange",
+                "order.tracking",
+                new OrderCreatedEvent(
+                        savedOrder.getId(),
+                        savedOrder.getUserId(),
+                        savedOrder.getStatus(),
+                        savedOrder.getTotalAmount(),
+                        mapToOrderItemDTOs(savedOrder.getItems()),
+                        savedOrder.getCreatedAt()
+                )
+        );
+
         return Optional.of(mapToOrderResponse(savedOrder));
+    }
+
+    private List<OrderItemDTO> mapToOrderItemDTOs(List<OrderItem> orderItems) {
+        return orderItems.stream()
+                .map(item -> new OrderItemDTO(
+                        item.getId(),
+                        item.getProductId(),
+                        item.getQuantity(),
+                        item.getPrice(),
+                        item.getPrice().multiply(new BigDecimal(item.getQuantity()))
+                )).toList();
     }
 
     private OrderResponse mapToOrderResponse(Order order) {
@@ -61,14 +90,7 @@ public class OrderService {
                 order.getId(),
                 order.getTotalAmount(),
                 order.getStatus(),
-                order.getItems().stream()
-                .map(item -> new OrderItemDTO(
-                        item.getId(),
-                        item.getProductId(),
-                        item.getQuantity(),
-                        item.getPrice(),
-                        item.getPrice().multiply(new BigDecimal(item.getQuantity()))
-                )).toList(),
+                mapToOrderItemDTOs(order.getItems()),
                 order.getCreatedAt());
     }
 
